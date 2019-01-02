09f3df096578ff0db5e20a1e288f004f99abb129
Jakub Postępski <jakub.postepski@gmail.com>
2019-01-02 13:39:15 +0100

    x

--- moro.m
+++ moro.m
@@ -1,32 +1,32 @@
 clear variables;
-rotmat = @(a, alpha, d, theta) [cos(theta), -sin(theta)*cos(alpha), sin(theta)*sin(alpha), a*cos(theta); 
-    sin(theta), cos(theta)*cos(alpha), -cos(theta)*sin(alpha), a*sin(theta);
-    0, sin(alpha), cos(alpha), d;
+rotmat = @(a, alpha, d, theta) [cos(theta), -sin(theta), 0, a; 
+    sin(theta)*cos(alpha), cos(theta)*cos(alpha), -sin(alpha), -d*sin(alpha);
+    sin(theta)*sin(alpha), cos(theta)*sin(alpha), cos(alpha), -d*cos(alpha);
     0, 0, 0, 1];
 
-syms a1 a2 a3 a4 theta1 theta2 theta3 theta4 theta5 theta6
-T01 = rotmat(0, 0, 0, theta1)
-T12 = rotmat(a1, -pi/2, 0, theta2)
-T23 = rotmat(a2, 0, 0, theta3)
-T34 = rotmat(a3, pi/2, a4, theta4)
-T45 = rotmat(0, pi/2, 0, theta5)
-T56 = rotmat(0, pi/2, 0, theta6)
+syms a_1 a_2 a_3 a_4 theta_1 theta_2 theta_3 theta_4 theta_5 theta_6
+T01 = rotmat(0, 0, 0, theta_1)
+T12 = rotmat(a_1, -pi/2, 0, theta_2)
+T23 = rotmat(a_2, 0, 0, theta_3)
+T34 = rotmat(a_3, pi/2, a_4, theta_4)
+T45 = rotmat(0, pi/2, 0, theta_5)
+T56 = rotmat(0, pi/2, 0, theta_6)
 
 
-T56(1, 2) = 0;
+T56(2, 1) = 0;
 T56(2, 2) = 0;
 T56(3, 3) = 0;
 
 
-T45(1, 2) = 0;
+T45(2, 1) = 0;
 T45(2, 2) = 0;
 T45(3, 3) = 0;
 
-T34(1, 2) = 0;
+T34(2, 1) = 0;
 T34(2, 2) = 0;
 T34(3, 3) = 0;
 
-T12(1, 2) = 0;
+T12(2, 1) = 0;
 T12(2, 2) = 0;
 T12(3, 3) = 0;
 
